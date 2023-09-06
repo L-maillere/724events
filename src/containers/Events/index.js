ec3f44d7efdfa1574497c4a17c2c7273eff4c9d1
Louis Maillere <louis.maillere@gmail.com>
2023-09-06 16:35:17 +0200

    Fix Filtre section Events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,11 +13,12 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
+  let filteredEvents = (
+    !type
       ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
+      : data?.events.filter(event => event.type === type)
+  ) || [];
+  filteredEvents = filteredEvents.filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
